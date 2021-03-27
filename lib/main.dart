030ab1fdb35dec6ad553729153d4ad6337b92714
Your Name <you@example.com>
2021-03-26 23:29:38 -0300

    Finish App

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,48 @@
-import 'package:flutter/material.dart';
 import 'dart:math';
+
+import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class Ball extends StatefulWidget {
+  @override
+  BallState createState() => BallState();
+}
+
+class BallState extends State<Ball> {
+  int ballnumber = 2;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blue,
+      alignment: Alignment.center,
+      child: FlatButton(
+        onPressed: () => {
+          setState(() => {
+                ballnumber = Random().nextInt(4) + 1,
+                print("I got Clicked = $ballnumber"),
+              })
+        },
+        child: Image.asset('images/ball$ballnumber.png'),
+      ),
+    );
+  }
+}
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Ask me Anything"),
+        backgroundColor: Colors.blue.shade900,
+      ),
+      body: Ball(),
+    );
+  }
+}
